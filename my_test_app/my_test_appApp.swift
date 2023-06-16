8c9f40f623651fee910cf576175335cc161cb846
reeli <reeli.null@gmail.com>
2023-06-16 16:06:44 +0800

    Initial Commit

---
+++ my_test_app/my_test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_test_appApp.swift
+//  my_test_app
+//
+//  Created by rrli  on 2023/6/16.
+//
+
+import SwiftUI
+
+@main
+struct my_test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
